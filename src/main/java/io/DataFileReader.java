66b48cf134f5d978eb66ba09ab301e7f52e313f4
Lan Jiang <jianglan1350@gmail.com>
2019-01-11 13:43:20 +0100

    add file reader and unioner classes

---
+++ src/main/java/io/DataFileReader.java
@@ -0,0 +1,8 @@
+package io;
+
+/**
+ * @author Lan Jiang
+ * @since 1/11/19
+ */
+public class DataFileReader {
+}
---
+++ src/main/java/union/DataFileUnioner.java
@@ -0,0 +1,8 @@
+package union;
+
+/**
+ * @author Lan Jiang
+ * @since 1/11/19
+ */
+public class DataFileUnioner {
+}
